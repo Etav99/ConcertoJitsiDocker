d466e3f29d1839c2a7d1561f4dde806aae080aeb
Maciej Zakrzewski <etavpl@gmail.com>
2023-04-03 15:13:22 +0200

    add default Concerto finalize script for jibri

---
+++ jibri/rootfs/defaults/finalize.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+RECORDINGS_DIR=$1
+
+meeting_url=$(cat $RECORDINGS_DIR/metadata.json | jq -r '.meeting_url')
+meeting_name=$(basename $meeting_url)
+recording_file=$(find $RECORDINGS_DIR -type f -name '*.mp4' | head -n 1)
+
+request="{ \"FilePath\": \"$recording_file\", \"MeetingId\": \"$meeting_name\", \"RecorderKey\": \"$JIBRI_RECORDER_PASSWORD\" }"
+echo $request
+(
+  curl --retry-connrefused --retry 10000 -s -w "\\n FinishRecording webhook result\\nResponse code: %{http_code}\\nSend to: %{url_effective}\\nMeeting ID: $meeting_name\\n\\n" -X POST \
+    -H "Content-Type: application/json" \
+    -d "$request" \
+    http://concerto_server/Storage/RecordingFinished
+) & disown
+
+echo "Finalize script for $meeting_name finished"
